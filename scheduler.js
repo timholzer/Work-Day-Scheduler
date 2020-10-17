0a8e10e2adc4629726c4a182ed8c8ee2ec9a6d9e
Tim Holzer <timiholzer@gmail.com>
2020-10-17 01:33:10 -0600

    connected css given to the html

--- scheduler.js
+++ scheduler.js
@@ -2,6 +2,11 @@ $( document ).ready(function() {
     console.log(moment().format('LL'));
 $("#currentDay").text(moment().format('LL'));
 
+//check if anything is in local storage
+//cycle through every hour?
+//check hour and change every hour's color
+//make on save click that text area gets saved to local storage and reload everything in local storage.
+
 
 
 
