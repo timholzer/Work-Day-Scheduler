9f04617abc6656f153998670dff8f25e1f666d7f
Tim Holzer <timiholzer@gmail.com>
2020-10-17 16:43:43 -0600

    got it to check if the hours is in the past, present, or future

--- scheduler.js
+++ scheduler.js
@@ -18,7 +18,6 @@ $( document ).ready(function() {
       };
 
     //matching the text with today's date
-    console.log(moment().format('LL'));
 $("#currentDay").text(moment().format('LL'));
 
 
@@ -28,6 +27,7 @@ if(!localStorage.getItem('scheduledEvents')) {
   } else {
 updateCalendar(JSON.parse(localStorage.getItem('scheduledEvents')))
   }
+  //and adds the text to the calender
 function updateCalendar(scheduledEvents){
     $(".calendar-row").each(function() {
         let theHour = $(this).children("div");
@@ -36,26 +36,52 @@ function updateCalendar(scheduledEvents){
 }
 
 
-
-
-
-
 //check hour and change every hour's color
+var counter = 1;
+for(const property in scheduledEvents) {
+  var textEntry = "#hour" + counter;
+  var timeId = "#time" + counter;
+  var presentHour = moment().hour();
+  var timeString = $(timeId).text();
+  var timeNumber = timeToNumber(timeString); 
+  if(timeNumber < presentHour) {
+    $(textEntry).addClass("past");
+  } else if (timeNumber > presentHour) {
+    $(textEntry).addClass("future");
+  } else {
+    $(textEntry).addClass("present");
+  }
+
+  counter ++;
 
 
+}
 
+//changing the hour to a number to compare with moment.js
 
+function timeToNumber(hourString) {
+    switch(hourString) {
+      case "6 AM": return 6;
+      case "7 AM": return 7;
+      case "8 AM": return 8;
+      case "9 AM": return 9;
+      case "10 AM": return 10;
+      case "11 AM": return 11;
+      case "12 PM": return 12;
+      case "1 PM": return 13;
+      case "2 PM": return 14;
+      case "3 PM": return 15;
+      case "4 PM": return 16;
+      case "5 PM": return 17;
+      case "6 PM": return 18;
+      case "7 PM": return 19;
+    }
+  }
 
-
-
-
-
-
-//makes on save click that text area gets saved to local storage
+//makes on save click that the text area gets saved to local storage
 $("button").click(function() {
     scheduleNotes = $(this).siblings("textarea").val();
-    hourString = $(this).siblings("div").text();
-    
+    hourString = $(this).siblings("div").text();   
     saveSchedule(hourString, scheduleNotes);
   });
 
@@ -69,10 +95,4 @@ $("button").click(function() {
     workHours[hourString] = scheduleNotes
     localStorage.setItem('scheduledEvents', JSON.stringify(workHours));
   }
-
- 
-
-
-
-
 });
